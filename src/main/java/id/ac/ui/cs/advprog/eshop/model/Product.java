060b6e042c77139b7884467d29759cdfa6110df6
OmarK2804 <khalifomar4@gmail.com>
2024-02-24 15:21:29 +0700

    Applying SOLID

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -2,11 +2,10 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import java.util.UUID;
 
 @Getter @Setter
 public class Product {
-    private String productId = UUID.randomUUID().toString();
+    private String productId;
     private String productName;
     private int  productQuantity;
 }
